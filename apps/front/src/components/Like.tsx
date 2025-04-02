dfbf000a5154a6b922129cbfdbd9e9d1dc0fbdfe
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2025-04-02 12:00:28 +0300

    Добавлена автоматическая повторная отправка запроса на сервер для обновления информации о лайках поста после успешного добавления или удаления "лайка" с помощью refetch хука useQuery и onSuccess хука useMutation (Tanstack Query).

--- apps/front/src/components/Like.tsx
+++ apps/front/src/components/Like.tsx
@@ -9,18 +9,20 @@ type Props = {
   postId: number
   user?: SessionUser
 }
-const Like = ({ postId, user }: Props) => {
-  const { data } = useQuery({
+const Like = ({ postId }: Props) => {
+  const { data, refetch: refetchPostLikesData } = useQuery({
     queryKey: ["FETCH_POST_LIKES_DATA", postId],
     queryFn: async () => await fetchPostLikesData(postId)
   })
 
   const setLikeToPostMutation = useMutation({
-    mutationFn: async () => await setLikeToPost(postId)
+    mutationFn: async () => await setLikeToPost(postId),
+    onSuccess: () => refetchPostLikesData()
   })
 
   const removeLikeFromPostMutation = useMutation({
-    mutationFn: async () => await removeLikeFromPost(postId)
+    mutationFn: async () => await removeLikeFromPost(postId),
+    onSuccess: () => refetchPostLikesData()
   })
   return (
     <div className="flex items-center justify-start mt-3 gap-2">
