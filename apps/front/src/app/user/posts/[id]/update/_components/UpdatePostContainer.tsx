5dd784aaeeaaa074645aab2ee31ff5ddb7c402b8
MAG1980 <al-1980@inbox.ru>
2025-04-14 01:28:57 +0300

    Рефакторинг. Типизирован хук useActionState в компоненте UpdatePostContainer.

--- apps/front/src/app/user/posts/[id]/update/_components/UpdatePostContainer.tsx
+++ apps/front/src/app/user/posts/[id]/update/_components/UpdatePostContainer.tsx
@@ -1,29 +1,29 @@
 'use client';
 import { updatePost } from '@/lib/actions/post';
 import { useActionState } from 'react';
-import { PostEntity } from "server/dist/src/post/entities/post.entity";
-import UpsertPostForm from "@/app/user/posts/_components/UpsertPostForm";
+import { PostEntity } from 'server/dist/src/post/entities/post.entity';
+import UpsertPostForm from '@/app/user/posts/_components/UpsertPostForm';
+import { PostFormState } from '@/lib/types';
 
 type Props = {
   post: PostEntity
 }
 
 function UpdatePostContainer({ post }: Props) {
-  //При первом рендере заполняем стейт формы данными поста, полученными из БД.
-  const [state, action] = useActionState(updatePost, {
+  const [state, action] = useActionState<PostFormState, FormData>(updatePost, {
     data: {
       postId: post.id,
       title: post.title,
       content: post.content,
       //Строка со ссылкой на предыдущее изображение.
-      previousThumbnail: post.thumbnail ?? undefined,
+      previousThumbnail: post.thumbnail,
       //Берем свойство name из каждого TagEntity в массиве тегов и объединяем их в строку.
       tags: post.tags?.map(tag => tag.name).join(', '),
       //Преобразуем логическое значение в строку, соответствующую состоянию checked или нет.
-      published: post.published ? 'on' : undefined
-    }
+      published: post.published,
+    },
   });
-  return <UpsertPostForm state={ state } formAction={ action }/>;
+  return <UpsertPostForm state={state} formAction={action} />;
 }
 
 export default UpdatePostContainer;
