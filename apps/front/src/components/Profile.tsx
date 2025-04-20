2de2eaf57161a3c187b02385f7d4a07d135c056a
MAG1980 <al-1980@inbox.ru>
2025-04-20 17:44:06 +0300

    В компоненте Profile настроено плавное появление и исчезновение контента в компоненте Popover (shadcn-ui/radix-ui) с помощью библиотеки Motion.

--- apps/front/src/components/Profile.tsx
+++ apps/front/src/components/Profile.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Popover, PopoverContent, PopoverTrigger } from '@radix-ui/react-popover';
+import { Arrow, Popover, PopoverContent, PopoverTrigger } from '@radix-ui/react-popover';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { ArrowRightStartOnRectangleIcon, ListBulletIcon, PencilSquareIcon, UserIcon } from '@heroicons/react/24/solid';
 import { SessionUser } from '@/lib/session/types';
@@ -8,6 +8,7 @@ import { useScreen } from 'usehooks-ts';
 import { useIsScrollDown } from '@/lib/hooks';
 import { cn } from '@/lib/utils';
 import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 type Props = {
   user: SessionUser
@@ -38,8 +39,8 @@ function Profile({ user }: Props) {
           </div>
         </div>)
       :
-      <Popover open={open}>
-        <PopoverTrigger onClick={() => setOpen(!open)}>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger>
           <Avatar className="w-8 h-8">
             <AvatarImage
               className={cn('border-white rounded-full  border-2 transition-colors duration-300', { 'border-blue-500': isScrollDown })}
@@ -52,41 +53,55 @@ function Profile({ user }: Props) {
             </AvatarFallback>
           </Avatar>
         </PopoverTrigger>
-        <PopoverContent className="md:text-white md:bg-gray-950 rounded-md z-10">
-
-          <div className="
+        <AnimatePresence>
+          {open && (
+            <PopoverContent forceMount
+                            className="md:text-white md:bg-gray-950 rounded-md z-10"
+                            asChild
+                            onClick={(open) => setOpen(!open)}
+                            onMouseLeave={(open) => setOpen(!open)}
+            >
+              <motion.div
+                key="content"
+                initial={{ opacity: 0, scale: 0.1, originX: '50%', originY: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0.1, originX: '50%', originY: 0 }}
+                transition={{
+                  duration: 0.5,
+                }}>
+                <Arrow></Arrow>
+                
+                <div className="
         max-w-52
-        rounded-md *:grid *:grid-cols-5 *:gap-3 *:items-center *:my-2 *:py-2 *:px-2
+        rounded-md *:grid *:grid-cols-5 *:gap-3 *:items-center *:py-2 *:px-2
         [&>*>span]:col-span-4
         [&>*:hover]:bg-sky-500
         *:transiton *:rounded-md
         [&>*>*:nth-child(1)]:justify-self-center
-        [&>*>span]:pr-4
-        "
-               onClick={(open) => setOpen(!open)}
-               onMouseLeave={(open) => setOpen(!open)}
-          >
-            <div className="flex justify-center items-center gap-3 py-2">
-              <UserIcon className="w-4" />
-              <p className="col-span-4 text-wrap">{user.name}</p>
-            </div>
-            {/*Если использовать Link,
+        [&>*>span]:pr-4">
+                  <div className="flex justify-center items-center gap-3 py-2">
+                    <UserIcon className="w-4" />
+                    <p className="col-span-4 text-wrap">{user.name}</p>
+                  </div>
+                  {/*Если использовать Link,
            то при выходе пользователя из системы страница не будет перезагружена,
            и будет отображаться кнопка "Выйти".*/}
-            <a href="/api/auth/sign-out">
-              <ArrowRightStartOnRectangleIcon className="w-4" />
-              <span>Выйти</span>
-            </a>
-            <Link href="/user/posts/create">
-              <PencilSquareIcon className="w-4" />
-              <span>Создать новый пост</span>
-            </Link>
-            <Link href="/user/posts">
-              <ListBulletIcon className="w-4" />
-              <span>Мои посты</span>
-            </Link>
-          </div>
-        </PopoverContent>
+                  <a href="/api/auth/sign-out">
+                    <ArrowRightStartOnRectangleIcon className="w-4" />
+                    <span>Выйти</span>
+                  </a>
+                  <Link href="/user/posts/create">
+                    <PencilSquareIcon className="w-4" />
+                    <span>Создать новый пост</span>
+                  </Link>
+                  <Link href="/user/posts">
+                    <ListBulletIcon className="w-4" />
+                    <span>Мои посты</span>
+                  </Link>
+                </div>
+              </motion.div>
+            </PopoverContent>)}
+        </AnimatePresence>
       </Popover>
   );
 }
