671d2ac4958773a9749a67f0d1a47699cc32b997
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2025-04-22 12:28:49 +0300

    Настроена анимация переходов между страницами приложения с помощью пакетов next-transition-router и Framer Motion.

--- apps/front/src/components/Providers.tsx
+++ apps/front/src/components/Providers.tsx
@@ -1,13 +1,45 @@
 'use client'
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useRef } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TransitionRouter } from "next-transition-router";
+import { animate } from 'framer-motion/dom';
 
 type Props = PropsWithChildren
 const queryClient = new QueryClient()
 const Providers = ({ children }: Props) => {
+
+  const wrapperRef = useRef<HTMLDivElement>(null)
   return (
-    <QueryClientProvider client={ queryClient }>{ children }</QueryClientProvider>
-  );
+    <TransitionRouter
+      auto
+      leave={ (next) => {
+        animate(
+          wrapperRef.current as unknown as object | object[],
+          {
+            opacity: [1, 0],
+            y: [0, "-100vh"]
+          },
+          { duration: 0.5, onComplete: next }
+        );
+      } }
+      enter={ (next) => {
+        animate(
+          wrapperRef.current as unknown as object | object[],
+          {
+            opacity: [0, 1],
+            y: ["100vh", 0]
+          },
+          { duration: 0.5, onComplete: next }
+        );
+      } }
+    >
+      <QueryClientProvider client={ queryClient }>
+        <div ref={ wrapperRef }>
+          { children }
+        </div>
+      </QueryClientProvider>
+    </TransitionRouter>
+  )
 }
 
 export default Providers;
